67209482e70bb34214da4f8dd455b83dff5fa869
PhuocNG0308 <71546982+PhuocNG0308@users.noreply.github.com>
2023-09-30 00:22:56 +0700

    Merge to main after week (Shop selected)

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -49,6 +49,9 @@ public class ShopManager : MonoBehaviour
 
         _randomSkinsData[5].IsUnlock = true;
         _randomSkinsData[3].IsUnlock = true;
+        _purchaseSkinsData[0].IsUnlock = true;
+        _tapEffectsData[0].IsUnlock = true;
+        _winEffectsData[0].IsUnlock = true;
     }
 
     public void FetchPurchaseSkinData(GameObject receiver)
